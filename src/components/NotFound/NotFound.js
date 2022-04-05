7c252b931e41ebf09f15f5d180de1fbc941fb66b
saiful489 <saifulsakil11@gmail.com>
2022-04-06 00:57:36 +0600

    404 page some chnages

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <div>
       {" "}
-      <h3>404 page not found</h3>
+      <h3>404 page is not found</h3>
       <p>We are sorry but the page you are looking for does not exist.</p>
     </div>
   );
